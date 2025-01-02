15f475b91a76c737f269dc2a49ea019f98534f59
Rabin Mainali <rabinmainali1999@gmail.com>
2025-01-02 16:31:24 +0545

    form reset functionality added in this component

---
+++ app/components/SearchFormReset.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { X } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+
+const SearchFormReset = () => {
+    
+        const reset = () =>{
+            const form = document.querySelector('.search-form') as HTMLFormElement
+            if(form) form.reset()
+        }
+  return (
+    <div>
+        <button type='reset' onClick={reset}>
+            <Link href="/" className='search-btn text-white'>
+                <X  size={20}/>
+            </Link>
+        </button>
+    </div>
+  )
+}
+
+export default SearchFormReset
