2d5e981ca3110f5c0623d04fa69a78fdf48361e6
Rabin Mainali <rabinmainali1999@gmail.com>
2025-01-02 16:33:12 +0545

    sending query props from searchParams to form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,21 @@
+import SearchForm from "../components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams}:{searchParams:Promise<{query?:string}>}) {
+  const query = (await searchParams).query
+
   return (
-    <div>Home page</div>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startups <br />
+          Connect With Entreprenaurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas , Vote On Pitches and Get Noticed In Virtual Competiton
+        </p>
+        <SearchForm query={query}/>
+      </section>
+    
+    </>
   )
 }
