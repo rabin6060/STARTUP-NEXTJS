5746b1ce458b6628b538a4caa4a073ba2c55b150
Rabin Mainali <rabinmainali1999@gmail.com>
2025-01-02 14:44:12 +0545

    navbar added in layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,9 @@
+import Navbar from "../components/Navbar";
 
 export default function  Layout({children}:Readonly<{children:React.ReactNode}>) {
     return (
         <main>
+            <Navbar/>
             {children}
         </main>
     )
