5efa084f55dd503e4f781369d81819775b83eae2
Rabin Mainali <rabinmainali1999@gmail.com>
2025-01-12 19:48:17 +0545

    simple util function to parse data

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,6 @@ export function formatDate(date:string){
   return new Date(date).toLocaleDateString('en-US',{month:'short',day:'numeric',year:'numeric'})
 } 
 
+export function parseResponse<T>(response:T){
+  return JSON.parse(JSON.stringify(response))
+}
