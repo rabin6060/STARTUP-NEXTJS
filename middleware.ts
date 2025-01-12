4f5214af63dc3c43fab040710975c90758ee95dd
Rabin Mainali <rabinmainali1999@gmail.com>
2025-01-12 12:58:39 +0545

    middleware to protect route based on session id

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,19 @@
-export { auth as middleware } from "@/auth"
+import { auth } from "@/auth";
+import { NextRequest, NextResponse } from "next/server";
+
+//middleware to access provided routes if only user is logged in!!
+export async function middleware(req:NextRequest){
+    const session = await auth()
+
+    if (!session?.id) {
+        const response = NextResponse.redirect(new URL('/',req.url))
+        response.cookies.set('unauthorize','true',{path:'/'})
+        return response
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher :[ '/startup/:id','/user/:id']
+}
